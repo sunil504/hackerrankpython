a03727fe111d86a7e3a3f056d14e6cfb07f7fc27
Sunil Digala <sunildigala@outlook.com>
2017-11-29 15:10:45 +0530

    Update NumpyArrays.py
    HackerRank Numpy Exercise 1

--- NumpyArrays.py
+++ NumpyArrays.py
@@ -1,2 +1,12 @@
 import numpy as np
+def arrays(arr):
+  x = []
+  for i in range(len(arr)):
+     x.append(arr[len(arr)-i-1])
+  return numpy.array(x,float)
+
+arr = input.strip.split(' ')
+result = arrays(arr)
+print(result)
+
 
